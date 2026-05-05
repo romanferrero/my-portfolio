a5627a8b7adb2738d9fd80f573e8997e53dcbdcd
Roman <romaferrero@hotmail.com>
2026-05-05 09:31:19 -0300

    working and fixing

--- src/context/ThemeContext.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * Deprecated barrel kept for backwards compatibility while the file
- * cannot be deleted from the sandbox. New code should import from:
- *   - ./themeContext.js     (ThemeContext, THEME_STORAGE_KEY)
- *   - ./ThemeProvider.jsx   (ThemeProvider)
- *
- * This file will be removed manually.
- */
-export { ThemeProvider } from "./ThemeProvider.jsx"
